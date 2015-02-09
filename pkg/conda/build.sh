b6c17ceab2f8a016179ff2783d2be7a3a8a300e9
Jelle Feringa <jelleferinga@gmail.com>
2015-02-09 19:20:49 +0100

    update conda build to support osx mavericks compiling with GCC, but not getting a stable working version

--- pkg/conda/build.sh
+++ pkg/conda/build.sh
@@ -1,18 +1,43 @@
 #! /bin/bash
 
-set -e
-
-ncpus=4
+ncpus=1
+if test -x /usr/bin/getconf; then
+    ncpus=$(/usr/bin/getconf _NPROCESSORS_ONLN)
+fi
 
 echo "Timestamp" && date
 mkdir cmake-build
 cd cmake-build
+
+OSX_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
+#      -DNETGEN_SUPPORT=ON \
+
+#      OCE_INCLUDE_PATH
+#      OCE_LIB_PATH
+
+#      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+
+
+#      -DCMAKE_MACOSX_RPATH=OFF \
+#      -DCMAKE_SKIP_RPATH=ON \
+#      -DBUILD_WITH_INSTALL_RPATH=OFF \
+#      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=On
+
 cmake -DCMAKE_BUILD_TYPE:STRING=Release \
-      -DOCE_DIR=$PREFIX/lib \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET= \  # horrible default values, pointing to 10.5 SDK
-      -DCMAKE_OSX_SYSROOT= \            # idem, awkward cmake default value... finds the right SDK when left open
+      -DCMAKE_CXX_COMPILER=$PREFIX/bin/c++ \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_OSX_SYSROOT=$OSX_SDK \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+      -DBoost_DIR=$PREFIX/share/cmake-3.0/Modules/ \
+      -DBoost_INCLUDE_DIR=$PREFIX/include \
+      -DOCE_DIR=$PREFIX/lib \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET= \
+      -DCMAKE_OSX_SYSROOT= \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DSMESH_TESTING=OFF \
+      -DCMAKE_MACOSX_RPATH=OFF \
       ..
+
 echo ""
 echo "Timestamp" && date
 # make for py3
