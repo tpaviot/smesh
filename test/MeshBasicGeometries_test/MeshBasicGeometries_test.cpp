30d582e760eb1eba2717146a03ca25f92496a24b
Thomas Paviot <tpaviot@gmail.com>
2017-02-22 04:55:12 +0100

    added a unittest for netgen support

--- test/MeshBasicGeometries_test/MeshBasicGeometries_test.cpp
+++ test/MeshBasicGeometries_test/MeshBasicGeometries_test.cpp
@@ -7,6 +7,9 @@
 #include <StdMeshers_AutomaticLength.hxx>
 #include <StdMeshers_TrianglePreference.hxx>
 #include <StdMeshers_MEFISTO_2D.hxx>
+#include <NETGENPlugin_NETGEN_2D.hxx>
+#include <NETGENPlugin_Hypothesis_2D.hxx>
+#include <NETGENPlugin_SimpleHypothesis_2D.hxx>
 #include <gtest/gtest.h>
 
 TEST(MeshBasicGeometriesSuite, testMeshBox)
@@ -98,6 +101,35 @@ TEST(MeshBasicGeometriesSuite, testMeshTorus)
     // set geometry to be meshed
     mesh->ShapeToMesh(my_torus.Shape());
     ASSERT_TRUE(mesh->HasShapeToMesh());
+    // compute the mesh
+    meshgen->Compute(*mesh, mesh->GetShapeToMesh());
+    // free memory
+    delete meshgen;
+    delete mesh;
+}
+
+TEST(MeshBasicGeometriesSuite, testMeshBoxNetgen)
+{
+    // create a box, mixing integers and floats
+    BRepPrimAPI_MakeBox my_box(10.,10.,10.);
+    my_box.Build();
+    ASSERT_TRUE(my_box.IsDone());
+    // create the Mesh
+    SMESH_Gen* meshgen = new SMESH_Gen();
+    SMESH_Mesh* mesh = meshgen->CreateMesh(0, true);
+    // set geometry to be meshed
+    mesh->ShapeToMesh(my_box.Shape());
+    ASSERT_TRUE(mesh->HasShapeToMesh());
+    // check bounding box. It should be 10.sqrt(3)==17.32050807568877
+    double diagonal_size = mesh->GetShapeDiagonalSize(mesh->GetShapeToMesh());
+    ASSERT_GT(diagonal_size, 17.320508);
+    ASSERT_LT(diagonal_size, 17.320509);
+    // create and add hypothesis
+    NETGENPlugin_Hypothesis_2D* hyp2d = new NETGENPlugin_Hypothesis_2D(0,0,meshgen);
+    hyp2d->SetFineness(NETGENPlugin_Hypothesis_2D::Fineness(3.0));
+    NETGENPlugin_NETGEN_2D* alg2d = new NETGENPlugin_NETGEN_2D(1,0,meshgen);
+    mesh->AddHypothesis(mesh->GetShapeToMesh(), 0);
+    mesh->AddHypothesis(mesh->GetShapeToMesh(), 1);
     // compute the mesh
     meshgen->Compute(*mesh, mesh->GetShapeToMesh());
     // free memory
