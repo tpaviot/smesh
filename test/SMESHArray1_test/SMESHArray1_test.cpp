f92ea45da28153d904cdd5f8332ad06bfba540be
tpaviot <tpaviot@gmail.com>
2017-06-26 05:04:58 +0200

    Removed SMESHArray1_test

--- test/SMESHArray1_test/SMESHArray1_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <SMESH_Array1.hxx>
-#include <SMESH_Array2.hxx>
-
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv){
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
